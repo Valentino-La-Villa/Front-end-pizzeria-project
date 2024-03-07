c56eca9f86fec6759a8a2bb885dbd9ade052077d
Valentino <duamnlavilla@gmail.com>
2024-03-07 14:46:07 -0300

    Added the functionality of closing the burger menu when the user clicks away

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import burgerMenuIcon from '../assets/icons/burgerMenuIcon.svg'
 import burgerMenuCloseX from '../assets/icons/burgerMenuCloseX.svg'
+import { ClickAwayListener } from '@mui/base/ClickAwayListener';
 
 export default function Navbar() {
 
@@ -36,43 +37,52 @@ export default function Navbar() {
                 <p>Contact</p></NavLink>
         </nav>
 
-        <nav                                                        // Burger menu display for -1200px / hidden in lower res
+
+
+        <nav                                                        // Burger menu display for -1200px / hidden in higher res
         className="col-3
                    col-sm-2
                    d-xl-none 
                    d-flex nav--burgerMenu-list-container justify-content-end ">
-            <button className="btn p-0" onClick={()=>{setBurgerMenuVisibility(true)}}>
-                <img className="img-fluid" style={{width: '50px'}} src={burgerMenuIcon}></img>
-            </button>
 
-            <sidebar className={dynamicBurgerMenuClassname}>
-                <ul className="nav--burgerMenu-list">
-                    <li>
-                        <button className="btn p-0" onClick={()=>{setBurgerMenuVisibility(false)}}>
-                            <img className="img-fluid" style={{width: '30px'}} src={burgerMenuCloseX}></img>
-                        </button>
-                    </li>
-                    <li>
-                        <NavLink className={defineStyle} to={'/'}>Home</NavLink>
-                    </li>
-                    
-                    <li>
-                        <NavLink className={defineStyle} to={'/about'}>About</NavLink>
-                    </li>
+            
 
-                    <li>
-                        <NavLink className={defineStyle} to={'/products'}>Products</NavLink>
-                    </li>
+            <ClickAwayListener onClickAway={()=>{setBurgerMenuVisibility(false)}}>
+                <div className="m-0 p-0 d-flex">
+                    <button className="btn p-0" onClick={()=>{setBurgerMenuVisibility(true)}}>
+                        <img className="img-fluid" style={{width: '50px'}} src={burgerMenuIcon}></img>
+                    </button>
 
-                    <li>
-                        <NavLink className={defineStyle} to={'/branches'}>Branches</NavLink>
-                    </li>
-                    
-                    <li>
-                        <NavLink className={defineStyle} to={'/contact'}>Contact</NavLink>
-                    </li>
-                </ul>
-            </sidebar>
+                    <main className={dynamicBurgerMenuClassname}>
+                        <ul className="nav--burgerMenu-list">
+                            <li>
+                                <button className="btn p-0" onClick={()=>{setBurgerMenuVisibility(false)}}>
+                                    <img className="img-fluid" style={{width: '30px'}} src={burgerMenuCloseX}></img>
+                                </button>
+                            </li>
+                            <li>
+                                <NavLink className={defineStyle} to={'/'}>Home</NavLink>
+                            </li>
+                            
+                            <li>
+                                <NavLink className={defineStyle} to={'/about'}>About</NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink className={defineStyle} to={'/products'}>Products</NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink className={defineStyle} to={'/branches'}>Branches</NavLink>
+                            </li>
+                            
+                            <li>
+                                <NavLink className={defineStyle} to={'/contact'}>Contact</NavLink>
+                            </li>
+                        </ul>
+                    </main>
+                </div>
+            </ClickAwayListener>
         </nav>
         </>
     )
